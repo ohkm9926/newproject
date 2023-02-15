064694f95874388c7b1346890a647d03b1ebf3db
kyungmin <ohkm9926@naver.com>
2023-02-15 15:16:57 +0900

    first upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [따봉, 따봉변경] = useState(0);
+  let font = {color : 'blue' , fontSize : '30px'}
+  let cont = '사자 고기 맛집'
+  let[글제목,글제목변경] = useState([' 남자 코트 추천' ,'강남 우동 맛집' , '파이썬 독학']);
+ // function 제목바꾸기(){
+  //  var newArrlay = [...글제목];
+  //  newArrlay[0] = '여자코트 추천';
+  //  글제목변경(newArrlay);
+ // }
+    return(
+    <div className="App">
+     <div className='black-nav'>
+<div>개발 Blog</div>
+      
+     </div>
+  
+
+     
+     <div className='list'>
+      <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉 +1)}}>❤</span>{따봉}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+       </div>
+       <div className='list'>
+      <h3>{글제목[1]}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+       </div>
+       <div className='list'>
+      <h3>{글제목[2]}</h3>
+      <p>2월 17일 발행</p>
+       <hr/>
+       </div>
+       <Modal/>
+     </div>
+    
+  
+  );
+}
+
+function Modal(){
+  return(
+    <div className='modal'>
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+    
+              </div>
+
+  )
+}
+
+export default App;
